fe71278ab46c57d8ec1e1ae16bbafb1c22e51126
Pavel Kirienko <pavel.kirienko@gmail.com>
2025-01-11 19:51:37 +0200

    Speedup x10

--- src/jafit/ja.py
+++ src/jafit/ja.py
@@ -94,6 +94,7 @@ def solve(
     tolerance: float = 1e-3,
     saturation_susceptibility: float = 0.1,
     H_stop_range: tuple[float, float] = (100e3, 3e6),
+    max_iter: int = 10**6,
     no_ascent: bool = False,
 ) -> Solution:
     """
@@ -118,51 +119,35 @@ def solve(
 
     def sweep_hmb(H: float, M: float, sign: int) -> npt.NDArray[np.float64]:
         assert sign in (-1, +1)
-        hm: list[tuple[float, float]] = [(H, M)]  # Keep the initial point for completeness.
-        dH_abs = 1e-3  # This is just a guess that will be dynamically refined.
-        for idx in itertools.count():
-            # Integrate using Heun's method (instead of Euler's) for better stability.
-            dH = sign * dH_abs
-            try:
-                dM_dH_1 = _dM_dH(c_r=c_r, M_s=M_s, a=a, k_p=k_p, alpha=alpha, H=H, M=M, direction=sign)
-                M_1 = M + dM_dH_1 * dH
-                dM_dH_2 = _dM_dH(c_r=c_r, M_s=M_s, a=a, k_p=k_p, alpha=alpha, H=H + dH, M=M_1, direction=sign)
-                M_2 = M + 0.5 * (dM_dH_1 + dM_dH_2) * dH
-            except (FloatingPointError, ZeroDivisionError) as ex:
-                if idx < 10:
-                    raise NumericalError(f"Numerical failure @#{idx}, H={H:+.6f}, M={M:+.6f}") from ex
-                _logger.warning(
-                    "Sweep stopped @#%s, H=%+.3f, M=%+.3f, dH=%+.6f due to numerical error: %s", idx, H, M, dH, ex
-                )
-                break
+        hmb = np.empty((max_iter, 3), dtype=np.float64)
+        hmb[0] = H, M, np.nan  # The initial point shall be set by the caller.
+        idx = np.array([0], dtype=np.uint32)
+        try:
+            status = _solve(
+                hm_out=hmb[:, :2],
+                idx_out=idx,
+                c_r=c_r,
+                M_s=M_s,
+                a=a,
+                k_p=k_p,
+                alpha=alpha,
+                direction=sign,
+                tolerance=tolerance,
+                saturation_susceptibility=saturation_susceptibility,
+                H_stop_range=H_stop_range,
+            )
+        except (FloatingPointError, ZeroDivisionError) as ex:
+            status = "Too few points in the sweep" if idx[0] < 10 else ""
+            H, M, _ = hmb[idx[0]]
+            _logger.warning("Sweep stopped at #%s, H=%+.3f, M=%+.3f due to numerical error: %s", idx[0], H, M, ex)
 
-            # Check if the tolerance is acceptable, only accept the data point if it is; otherwise refine and retry.
-            if np.abs(M_1 - M_2) > tolerance:
-                dH_abs /= 2
-                if dH_abs <= (_EPSILON * 100):
-                    raise ConvergenceError(
-                        "Step size too small: "
-                        f"idx={idx}, H={H:+.6f}, M={M:+.6f}, dH={dH:.9f}, dM_dH_1={dM_dH_1:.9f}, dM_dH_2={dM_dH_2:.9f}"
-                    )
-                continue
-
-            dH_abs = min(dH_abs * 1.1, 1e3)
-            H, M = H + dH, M_2
-            hm.append((H, M))
-
-            # Termination check and logging.
-            chi = np.abs((hm[-1][1] - hm[-2][1]) / (hm[-1][0] - hm[-2][0])) if len(hm) > 1 else np.inf
-            if (sign > 0) == (H > 0) and chi < saturation_susceptibility and H * sign >= H_stop_range[0]:
-                _logger.debug(f"Sweep stopped @#{idx}, H={H:+.3f}, M={M:+.3f} due to saturation: x={chi:.6f}")
-                break
-
-            if H * sign > H_stop_range[1]:
-                _logger.debug(f"Sweep stopped @#{idx}, H={H:+.3f}, M={M:+.3f} due to H magnitude limit")
-                break
-
-        out = np.array([(h, m, mu_0 * (h + m)) for h, m in hm], dtype=np.float64)
-        out.setflags(write=False)
-        return out
+        hmb = hmb[: idx[0] + 1]
+        hmb[:, 2] = mu_0 * (hmb[:, 0] + hmb[:, 1])
+        hmb.setflags(write=False)
+        H, M, _ = hmb[-1]
+        if status:
+            raise ConvergenceError(f"Convergence failure at #{idx[0]}, H={H:+.3f}, M={M:+.3f}: {status}")
+        return hmb
 
     hmb_virgin = sweep_hmb(0, 0, +1)
 
@@ -175,6 +160,70 @@ def solve(
         hmb_maj_asc = sweep_hmb(H_last, M_last, +1)
 
     return Solution(HMB_virgin=hmb_virgin, HMB_major_descending=hmb_maj_dsc, HMB_major_ascending=hmb_maj_asc)
+
+
+@njit(nogil=True)
+def _solve(
+    hm_out: npt.NDArray[np.float64],
+    idx_out: npt.NDArray[np.uint32],
+    c_r: float,
+    M_s: float,
+    a: float,
+    k_p: float,
+    alpha: float,
+    direction: int,
+    tolerance: float,
+    saturation_susceptibility: float,
+    H_stop_range: tuple[float, float],
+) -> str:
+    """
+    This function is very bare-bones because it has to be compilable in the nopython mode.
+    We can't handle exceptions, can't log, etc, just numerical stuff; the flip side is that it is super fast.
+    The caller needs to jump through some extra hoops to use this function, but it is worth it.
+    """
+    assert direction in (-1, +1)
+    assert len(hm_out.shape) == 2 and (hm_out.shape[1] == 2)
+    assert idx_out.shape == (1,)
+    assert idx_out[0] == 0
+
+    dH_abs = 1e-3  # This is just a guess that will be dynamically refined.
+    max_iter = hm_out.shape[0]
+    assert np.isfinite(hm_out[0]).all()  # We require that the first point is already filled in.
+    while idx_out[0] < max_iter:
+        dH = direction * dH_abs
+        H, M = hm_out[idx_out[0]]
+
+        # Integrate using Heun's method (instead of Euler's) for better stability.
+        # _dM_dH() may throw FloatingPointError or ZeroDivisionError; we can't handle them in the compiled code.
+        # Since we always update the current state in the caller's context, we can still return partial solution
+        # even if an exception is raised.
+        dM_dH_1 = _dM_dH(c_r=c_r, M_s=M_s, a=a, k_p=k_p, alpha=alpha, H=H, M=M, direction=direction)
+        M_1 = M + dM_dH_1 * dH
+        dM_dH_2 = _dM_dH(c_r=c_r, M_s=M_s, a=a, k_p=k_p, alpha=alpha, H=H + dH, M=M_1, direction=direction)
+        M_2 = M + 0.5 * (dM_dH_1 + dM_dH_2) * dH
+
+        # Check if the tolerance is acceptable, only accept the data point if it is; otherwise refine and retry.
+        if np.abs(M_1 - M_2) > tolerance:
+            dH_abs *= 0.5
+            if dH_abs < 1e-8:
+                return "Convergence failure: error still large at the smallest step size"
+            continue
+
+        # Save the next point. This ensures that the caller will get partial results even if an exception is raised.
+        idx = idx_out[0] + 1
+        idx_out[0] = idx
+        dH_abs = min(dH_abs * 1.1, 1e3)
+        hm_out[idx] = H + dH, M_2
+
+        # Termination check and logging. It is guaranteed that we have at least two points now.
+        # No need to log the termination reason here because it is easily inferrable from the output.
+        chi = np.abs((hm_out[idx][1] - hm_out[idx - 1][1]) / (hm_out[idx][0] - hm_out[idx - 1][0]))
+        if H * direction >= H_stop_range[0] and chi < saturation_susceptibility:
+            return ""
+        if H * direction > H_stop_range[1]:
+            return ""
+
+    return "Maximum number of iterations reached"
 
 
 @njit(nogil=True)
