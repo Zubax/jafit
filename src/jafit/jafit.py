331f92178c7a23dfb8fbb75f4a869943656dc4b4
Pavel Kirienko <pavel.kirienko@gmail.com>
2025-01-20 20:29:22 +0200

    Heuristic

--- src/jafit/jafit.py
+++ src/jafit/jafit.py
@@ -175,7 +175,7 @@ def do_fit(
                 H_c * 2,
                 M_s_min * 0.1,
             )
-        ), float(H_max or max(M_s_min * 2, H_c * 4))
+        ), float(H_max or max(100e3, M_s_min * 2, H_c * 4))
     _logger.info("H amplitude range: %s [A/m]", H_stop)
 
     # Run the optimizer.
